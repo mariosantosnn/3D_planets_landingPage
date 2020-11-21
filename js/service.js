a627b46a15f7ad608b15c9fa90e1ba1905e4d24e
Mario Santos <mariosantosnn@gmail.com>
2020-11-21 16:29:56 +0000

    individual service page almost done

--- js/service.js
+++ js/service.js
@@ -1,37 +1,24 @@
 //Movement Animation to happen
-const card = document.querySelector(".card");
-const container = document.querySelector(".container");
+const contentor = document.querySelector(".contentor");
 //Items
-const title = document.querySelector(".title");
-const sneaker = document.querySelector(".sneaker img");
-const purchase = document.querySelector(".purchase");
-const description = document.querySelector(".info h3");
-const sizes = document.querySelector(".sizes");
+const first = document.querySelector(".first");
+const second = document.querySelector(".second");
+const third = document.querySelector(".third");
 
-//Moving Animation Event
-container.addEventListener("mousemove", (e) => {
-  let xAxis = (window.innerWidth / 2 - e.pageX) / 25;
-  let yAxis = (window.innerHeight / 2 - e.pageY) / 25;
-  card.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
-});
 //Animate In
-container.addEventListener("mouseenter", (e) => {
-  card.style.transition = "none";
+contentor.addEventListener("mouseenter", (e) => {
   //Popout
-  title.style.transform = "translateZ(150px)";
-  sneaker.style.transform = "translateZ(200px) rotateZ(-45deg)";
-  description.style.transform = "translateZ(125px)";
-  sizes.style.transform = "translateZ(100px)";
-  purchase.style.transform = "translateZ(75px)";
+  second.style.transition = "all 1s ease"
+  first.style.transition = "all 1s ease"
+  third.style.transition = "all 1s ease"
+  first.style.transform = "translateX(100px)";
+  second.style.transform = "translateX(150px) translateY(-50px) rotateZ(60deg)";
+  third.style.transform = "translateX(50px)";
 });
 //Animate Out
-container.addEventListener("mouseleave", (e) => {
-  card.style.transition = "all 0.5s ease";
-  card.style.transform = `rotateY(0deg) rotateX(0deg)`;
+contentor.addEventListener("mouseleave", (e) => {
   //Popback
-  title.style.transform = "translateZ(0px)";
-  sneaker.style.transform = "translateZ(0px) rotateZ(0deg)";
-  description.style.transform = "translateZ(0px)";
-  sizes.style.transform = "translateZ(0px)";
-  purchase.style.transform = "translateZ(0px)";
+  first.style.transform = "translateZ(0px)";
+  second.style.transform = "translateZ(0px) rotateZ(0deg)";
+  third.style.transform = "translateZ(0px)";
 });
