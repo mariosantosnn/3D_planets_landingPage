500c32af0a714cfe9a952d77630f7f3d39b4c7fe
Mario Santos <mariosantosnn@gmail.com>
2021-01-16 16:11:01 +0000

    testing responsive features

--- js/home_worlds.js
+++ js/home_worlds.js
@@ -101,21 +101,6 @@ import * as THREE from '/js/three.module.js';
 
 			}
 
-// create an AudioListener and add it to the camera
-var listener = new THREE.AudioListener();
-camera.add( listener );
-
-// create a global audio source
-var sound = new THREE.Audio( listener );
-
-// load a sound and set it as the Audio object's buffer
-audioLoader.load( '/assets/sounds/entry.wav', function( buffer ) {
-	sound.setBuffer( buffer );
-	sound.setLoop( true );
-	sound.setVolume( 0.5 );
-	sound.play();
-	});
-
 			function animate() {
 
 				requestAnimationFrame( animate );
